19e567e836e99d0742ddcb03cd9430018178be71
Sarbjit Singh <sarbjeets18@gmail.com>
2021-08-23 15:05:40 +0100

    search bar added

--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -12,6 +12,7 @@ state={
  movies: [], 
  selectedPage: 1,
  pageSize:4,
+ searchtext: '',
  selectedGenre:'',
  sortedColumn: {path:'title', order: 'asc'}
 }
@@ -26,19 +27,27 @@ handlePageChange = (pageNum)=>{
     this.setState({selectedPage: pageNum})
 }
 
+searchOnChange = ({currentTarget}) => {
+  this.handleGenreSelect('')  //reset genre filter and pagination 
+  this.setState({searchtext: currentTarget.value})
+ }
 
 
 //filter movies by using genre 
 handleGenreFilter = ()=> {
-  if(this.state.selectedGenre ==='' || this.state.selectedGenre ==='All Genres')
-   return this.state.movies 
+  if(this.state.selectedGenre ==='' || this.state.selectedGenre ==='All Genres'){
+    if(!(this.state.searchtext===''))  //filter movies using search bar .
+      return this.state.movies.filter(m=> 
+          m.title.toLowerCase().startsWith(this.state.searchtext.toLowerCase()))
+    return this.state.movies 
+  }
    return this.state.movies.filter(m=>
           m.genre.name===this.state.selectedGenre)
 }
 
   /* when user click on any genre button then pagination go back to 
     1st page that why selectedPage:1 */  
-handleGenreSelect = (genre) => this.setState({selectedGenre:genre, selectedPage:1})
+handleGenreSelect = (genre) => this.setState({selectedGenre:genre, selectedPage:1, searchtext:''})
   
 
 //save sortedcolumn data to this.state sortedcolumn={path:'title', order:'asc'}
@@ -55,7 +64,7 @@ displayMoviesCount = (movies)=> {
 componentDidMount(){
    this.setState({movies: getMovies()})
 }
-    render() {
+    render() { 
         //obect destructuring 
         const {movies, selectedPage, pageSize,selectedGenre ,sortedColumn } = this.state
         const filtered = this.handleGenreFilter() //get filtered data
@@ -79,6 +88,17 @@ componentDidMount(){
                 <Link className="btn btn-primary mb-4" to="/movies/new">Add Movie</Link> 
 
               <h3>{this.displayMoviesCount(movies)}</h3>
+
+              <div className="mb-3"> 
+              <input name='search'
+                 id='search' className="form-control"
+                 autoComplete="off"
+                 placeholder = 'Search..'
+                 value = {this.state.searchtext}
+                 onChange = {this.searchOnChange} 
+                 
+             />
+             </div>
                <MoviesTable 
                  deleteMovie={this.deleteMovie} 
                  movies = {paginateItems}
