1db477e0ef0acd6a03433bcafb263faf03d68dee
Sarbjit Singh <sarbjeets18@gmail.com>
2021-09-05 12:46:04 +0100

    movie page is not responsive

--- src/component/common/table.jsx
+++ src/component/common/table.jsx
@@ -1,32 +1,28 @@
-import React, { Component } from 'react'
-import TableBody from './tableBody'
-import TableHeader from './tableHeader'
+import React, { Component } from "react";
+import TableBody from "./tableBody";
+import TableHeader from "./tableHeader";
 
 export default class Table extends Component {
+  raiseSort = (path) => {
+    const { sortedColumn } = this.props;
+    if (sortedColumn.path === path)
+      sortedColumn.order = sortedColumn.order === "asc" ? "desc" : "asc";
+    else sortedColumn.path = path;
+    this.props.onSort(sortedColumn); //pass sortedColumn rather than path
+  };
 
-    raiseSort = (path)=>{
-        const {sortedColumn} = this.props  
-        if(sortedColumn.path === path)
-          sortedColumn.order= (sortedColumn.order ==='asc') ? 'desc' : 'asc'
-        else
-         sortedColumn.path = path
-      this.props.onSort(sortedColumn) //pass sortedColumn rather than path 
-     }
-     
-    render() {
-        const {columns, sortedColumn, deleteItem, data} = this.props
-        return (
-            <table className="table table-bordered">
-            <TableHeader columns={columns} 
-               sortedColumn ={sortedColumn} 
-               onSort ={this.raiseSort} />
+  render() {
+    const { columns, sortedColumn, deleteItem, data } = this.props;
+    return (
+      <table className="table table-bordered">
+        <TableHeader
+          columns={columns}
+          sortedColumn={sortedColumn}
+          onSort={this.raiseSort}
+        />
 
-           <TableBody 
-              columns ={columns} 
-              deleteRow ={deleteItem}
-              data={data}
-              />
-           </table>
-        )
-    }
+        <TableBody columns={columns} deleteRow={deleteItem} data={data} />
+      </table>
+    );
+  }
 }
